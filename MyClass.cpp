587dc6e84a70a906073818976e2347b453d9ce26
Unionc0de <144795080+Unionc0de@users.noreply.github.com>
2024-06-10 14:53:15 +0300

    Add files via upload

---
+++ MyClass.cpp
@@ -0,0 +1,7 @@
+//Реализация методов класса 
+#include "MyClass.h"
+using namespace std;
+void MyClass::showMessage(const char str[])//Реализация метода 
+{
+	cout << str << endl;
+}
---
+++ MyClass.h
@@ -0,0 +1,10 @@
+//Реализация сигнатуры класса 
+#pragma once
+#include <iostream>
+#include <string>//Библиотеки импортируются в файле заголовка
+class MyClass
+{
+public:
+	void showMessage(const char str[]);//Сигнатура метода 
+};
+
