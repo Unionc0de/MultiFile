bd77bc6ec7d34224156fba80a25a3668f1f89137
Unionc0de <144795080+Unionc0de@users.noreply.github.com>
2024-06-10 14:35:50 +0300

    Add files via upload

---
+++ MultiFile.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+#include "Summ.h"//Подключаем файл 
+#define random(min,max) (min + rand() % (max - min + 1))
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    srand(time(NULL));
+    cout << summa(1, 1);
+}
